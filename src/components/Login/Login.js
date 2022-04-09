ac8a93371d00a92272d223c2018c03ed09c183a0
Hossen <mdmitho199911@gmail.com>
2022-04-09 15:59:52 +0600

    auth reditect

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,18 +1,29 @@
 import { getAuth } from 'firebase/auth';
 import React from 'react';
 import {useSignInWithGoogle} from 'react-firebase-hooks/auth'
+import { useLocation, useNavigate } from 'react-router-dom';
 import app from '../../firebase.init'
 
 
 const auth = getAuth(app)
 const Login = () => {
-
+     const location =useLocation()
+     const navigate = useNavigate()
     const [signInWithGoogle, user] = useSignInWithGoogle(auth)
+
+const from = location?.state?.from?.pathname || '/'
+
+    const handleGoogleSingIn = () =>{
+        signInWithGoogle ()
+        .then (() => {
+            navigate(from,{replace:true})
+        })
+    }
     return (
         <div>
             <h1>Please login</h1>
             <div className="" style={{margin:"20px"}}>
-                <button onClick={() => signInWithGoogle()}>Google Sing In</button>
+                <button onClick={handleGoogleSingIn}>Google Sing In</button>
             </div>
             <form action="">
                 <input type="email" placeholder='Your Email'/>
