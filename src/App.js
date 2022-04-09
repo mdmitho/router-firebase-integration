d749df5fff3a91b7c3f7589e6f8f56adc6abd408
Hossen <mdmitho199911@gmail.com>
2022-04-09 15:45:47 +0600

    Introduction to Protected Route and Require Auth

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
+import Order from './components/Order/Order';
 import Products from './components/Products/Products';
 import Register from './components/Register/Register';
+import RequireAuth from './components/RequireAuth/RequireAuth';
 
  
 function App() {
@@ -17,6 +19,11 @@ function App() {
   <Route path='/login' element={<Login></Login>}></Route>
   <Route path='/register' element={<Register></Register>}></Route>
   <Route path='/products' element={<Products></Products>}></Route>
+  <Route path='/orders' element={
+    <RequireAuth>
+      <Order></Order>
+    </RequireAuth>
+  }></Route>
 </Routes>
 
     </div>
